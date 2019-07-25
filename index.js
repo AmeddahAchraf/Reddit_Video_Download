0191480148e8999d8779af81319413d949da20b8
Paulo <pauloeduardo2709@gmail.com>
2019-07-25 16:15:55 -0300

    ✨ Add output folder and post url as argv; Fix the download of posts without audio;

--- index.js
+++ index.js
@@ -2,51 +2,76 @@ const ffmpeg = require("fluent-ffmpeg");
 const proc = new ffmpeg();
 const urlRegex = require("url-regex");
 const fetch = require("node-fetch");
-var oneDownload = false ;
-//Change the URL Below
-const url = "https://www.reddit.com/r/BrawlStarsClips/comments/bpk3ak/id_like_to_nominate_my_teams_primo_for_best_play/";
+var oneDownload = false;
+
+if (process.argv.length <= 2) {
+  console.log('Missing URL.');
+  console.log('Usage: node index.js <reddit-url> [output-folder]');
+  console.log('(the default output folder is ./');
+  console.log('Example: node index.js https://www.reddit.com/r/ItHadToBeBrazil/comments/chjfh1/i_wonder_how_much_that_upgrade_costed/ ~/Videos');
+  process.exit(0);
+}
+
+const url = process.argv[2];
+
+var outputFolder = './';
+
+if (process.argv.length >= 4) outputFolder = process.argv[3];
+
+if (!outputFolder.endsWith('/')) outputFolder += '/';
+
 const _res = [1080, 720, 480,'2_4_M','1_2_M'];
 
-fetch(url)
-    .then(res => {
-        return res.text();
-    })
-    .then(body => {
-        const urls = body.match(urlRegex());
-        const mediaUrls = urls.filter(url => url.includes("v.redd.it"));
-        let mediaUrl = mediaUrls[0].split("https://v.redd.it/")[1];
-        mediaId = mediaUrl.split("/")[0];
-        testUrls(mediaId);
+console.log(`Output folder > ${outputFolder}`);
+console.log(`URL > ${url}`);
 
-    });
+fetch(url)
+  .then(res => {
+    return res.text();
+  })
+  .then(body => {
+    const urls = body.match(urlRegex());
+    const mediaUrls = urls.filter(url => url.includes("v.redd.it"));
+    let mediaUrl = mediaUrls[0].split("https://v.redd.it/")[1];
+    mediaId = mediaUrl.split("/")[0];
+    testUrls(mediaId);
+
+  });
 
 function testUrls(mediaId) {
-    console.log('Hold on, Fetching the Best Quality')
-    _res.forEach(res=>{
-        fetch(`https://v.redd.it/${mediaId}/DASH_${res}`)
-        .then(response =>{
-           if(response.status===200 && !oneDownload) {
-                oneDownload  = true ;
-                console.log('Downloading With : '+res +' Please Wait ...');
-                scrape(mediaId,res)
-
-                // TODO : Break From The Promise Then The For Loop
-                // Recursion 
-            }
-        })
-    })
+  console.log('Hold on, Fetching the Best Quality');
+  _res.forEach(res => {
+    fetch(`https://v.redd.it/${mediaId}/DASH_${res}`)
+      .then(response => {
+        if (response.status === 200 && !oneDownload) {
+          oneDownload = true;
+          console.log('Downloading With : ' + res + ' Please Wait ...');
+          scrape(mediaId, res);
+        }
+      });
+  });
 }
 
 
-function scrape(mediaId,res) {
-    proc.addInput(`https://v.redd.it/${mediaId}/DASH_${res}`)
-        .addInput(`https://v.redd.it/${mediaId}/audio`)
-        .output(`./${mediaId}-${res}.mp4`)
-        .on("error", err => {
-            console.log("Error: " + err);
-        })
-        .on("end", () => {
-            console.log("Done");
-        })
-        .run();
+function scrape(mediaId, res) {
+  proc.addInput(`https://v.redd.it/${mediaId}/DASH_${res}`)
+    .output(`${outputFolder}${mediaId}-${res}.mp4`)
+    .on("error", err => {
+      console.log("Error: " + err);
+    })
+    .on("end", () => {
+      console.log("Done");
+    });
+
+  fetch(`https://v.redd.it/${mediaId}/audio`)
+    .then(resp => {
+      if (resp.status === 200) {
+        console.log('Founded audio track...')
+        proc.addInput(`https://v.redd.it/${mediaId}/audio`);
+      } else {
+        console.log('No audio track...');
+      }
+      console.log('Downloading and converting...');
+      proc.run();
+    });
 }
